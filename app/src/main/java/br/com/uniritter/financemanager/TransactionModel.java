fcebdc770b02a42e3feadbb7e4e456c7cc7de49b
Fabrício <fabricio_c_anelli@hotmail.com>
2023-05-28 19:00:46 -0300

    feat: Adicionando Model e Adapter

---
+++ app/src/main/java/br/com/uniritter/financemanager/TransactionModel.java
@@ -0,0 +1,50 @@
+package br.com.uniritter.financemanager;
+
+public class TransactionModel {
+    private String id;
+    private String note;
+    private String amount;
+    private String type;
+
+    public TransactionModel() {
+    }
+
+    public TransactionModel(String id, String note, String amount, String type) {
+        this.id = id;
+        this.note = note;
+        this.amount = amount;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
